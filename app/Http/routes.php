114450b793113bc874411387de7e3417f2a2b67f
Arliton Rocha <arliton@gmail.com>
2015-07-25 03:13:44 -0300

    Fase 3 Projeto - Criação das rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,5 +15,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/categories', 'AdminCategoriesController@index');
-Route::get('admin/products', 'AdminProductsController@index');
+Route::pattern('id', '[0-9]+');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::group(['prefix' => 'categories'], function() {
+        Route::get('', ['as' => 'admin.categories.index', 'uses' => 'AdminCategoriesController@index']);
+        Route::get('create', ['as' => 'admin.categories.create', 'uses' => 'AdminCategoriesController@create']);
+        Route::post('store', ['as' => 'admin.categories.store', 'uses' => 'AdminCategoriesController@store']);
+        Route::get('edit/{id}', ['as' => 'admin.categories.edit', 'uses' => 'AdminCategoriesController@edit']);
+        Route::put('update/{id}', ['as' => 'admin.categories.update', 'uses' => 'AdminCategoriesController@update']);
+        Route::get('destroy/{id}', ['as' => 'admin.categories.destroy', 'uses' => 'AdminCategoriesController@destroy']);
+    });
+
+    Route::group(['prefix' => 'products'], function() {
+        Route::get('', ['as' => 'admin.products.index', 'uses' => 'AdminProductsController@index']);
+        Route::get('create', ['as' => 'admin.products.create', 'uses' => 'AdminProductsController@create']);
+        Route::post('store', ['as' => 'admin.products.store', 'uses' => 'AdminProductsController@store']);
+        Route::get('edit/{id}', ['as' => 'admin.products.edit', 'uses' => 'AdminProductsController@edit']);
+        Route::put('update/{id}', ['as' => 'admin.products.update', 'uses' => 'AdminProductsController@update']);
+        Route::get('destroy/{id}', ['as' => 'admin.products.destroy', 'uses' => 'AdminProductsController@destroy']);
+    });
+});
